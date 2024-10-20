6071bc105417abc1fa06983184502ca13fbcf745
SadPig69 <grakhova69@gmail.com>
2024-10-20 19:43:46 +0300

    Add files via upload

---
+++ horoscopeAPI/horoscopeAPI/src/main/java/ru/horoscope/horoscopeAPI/HoroscopeApiApplication.java
@@ -0,0 +1,13 @@
+package ru.horoscope.horoscopeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoroscopeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoroscopeApiApplication.class, args);
+	}
+
+}
